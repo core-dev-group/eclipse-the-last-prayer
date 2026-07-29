3a2c314d4a2529aa4c7953ec0e2ea4b4efe28775
kodel-dev <muhammadamin140904@gmail.com>
2026-07-30 03:52:22 +0700

    feat: initial commit for The Nameless King landing page

---
+++ main.js
@@ -0,0 +1,9 @@
+// Efek blur/transparan pada navbar saat di-scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
