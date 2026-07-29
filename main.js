fa32abbe1e08d4e316454fded86d9e2af718c7fd
kodel-dev <muhammadamin140904@gmail.com>
2026-07-30 04:05:33 +0700

    feat: complete redesign for AAA game aesthetic

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Efek blur/transparan pada navbar saat di-scroll
+// Efek Scroll Navbar
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) {
@@ -7,3 +7,22 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
+
+// Efek Reveal / Muncul saat discroll (Scroll Animation)
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100; // Mulai muncul saat elemen berjarak 100px dari bawah layar
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+// Jalankan saat pertama kali *load* dan setiap kali di-*scroll*
+window.addEventListener("scroll", reveal);
+reveal(); // Trigger saat halaman pertama dibuka agar Hero Section langsung muncul
